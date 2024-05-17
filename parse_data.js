d36f5e73fec22611c41c6e94dcdab6172bbf8a4e
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-05-17 18:39:14 -0400

    Update parse_data.js

--- parse_data.js
+++ parse_data.js
@@ -39,6 +39,7 @@ function createObjects(data) {
 
 function generateImageFilename(rarity, name, color) {
     const normalizedRarity = rarity.toLowerCase();
+    // this line checks first to make sure name is defined  
     const normalizedName = name ? name.toLowerCase().replace(/ /g, '').replace(/:/g, '').replace(/\(/g, '').replace(/\)/g, '') : '';
     const normalizedColor = color.toLowerCase();
     return normalizedColor + normalizedRarity + normalizedName + '.png';
