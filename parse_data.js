5a083d6bcac3ea89f2c74c4fcfab58b3448dd6c0
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-05-17 18:09:31 -0400

    Update parse_data.js

--- parse_data.js
+++ parse_data.js
@@ -1,14 +1,14 @@
 <script>
     class Fighter {
         constructor(rarity, name, color, tags, ztags, img, epi, dbl) {
-            this.rarity = rarity
-            this.name = name
-            this.color = color
-            this.tags = tags
-            this.ztags = ztags
-            this.img = img
-            this.epi = epi
-            this.dbl = dbl
+            this.rarity = rarity;
+            this.name = name;
+            this.color = color;
+            this.tags = tags;
+            this.ztags = ztags;
+            this.img = img;
+            this.epi = epi;
+            this.dbl = dbl;
         }
     }
 
@@ -30,13 +30,18 @@
     function createObjects(data) {
         const objects = [];
         for (let i = 1; i < data.length; i++) { // Start from index 1 to skip header row
-            const [rarity, name, color, tags, ztags, epi, dbl] = [data[i][0],data[i][1],data[i][2],data[i][3].split("."),data[i][4].split("."),data[i][5],"DBL".concat(data[i][6])]
-            const img = string.concat(color.toLowerCase(), rarity.toLowerCase(),
-                                      name.toLowerCase().replace("/ /g", "").replace("/:/g", "").replace("/(/g", "").replace("/)/g", ""),
-                                      ".png") 
-            const fighter = new Fighter(rarity, name, color, tags, ztags, img, epi, dbl); // Create fighter object
+            const [rarity, name, color, tags, ztags, epi, dbl] = data[i];
+            const img = generateImageFilename(rarity, name, color);
+            const fighter = new Fighter(rarity, name, color, tags.split("."), ztags.split("."), img, epi, "DBL" + dbl);
             objects.push(fighter);
         }
         return objects;
+    }
+
+    function generateImageFilename(rarity, name, color) {
+        const normalizedRarity = rarity.toLowerCase();
+        const normalizedName = name.toLowerCase().replace(/ /g, '').replace(/:/g, '').replace(/\(/g, '').replace(/\)/g, '');
+        const normalizedColor = color.toLowerCase();
+        return normalizedColor + normalizedRarity + normalizedName + '.png';
     }
 </script>
