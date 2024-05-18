f0a56a5090f7059d4d4fb980b6381ebd7c7f5db1
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-05-17 23:25:59 -0400

    Update episode_dropdown.js

--- episode_dropdown.js
+++ episode_dropdown.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const input = document.getElementById('dropdown-input');
-    const dropdownList = document.getElementById('dropdown-list');
-    const selectedOptionsContainer = document.getElementById('selected-options');
-    const toggleAllCheckbox = document.getElementById('toggle-all');
+    const input = document.querySelector('.episode-dropdown .dropdown-input');
+    const dropdownList = document.querySelector('.episode-dropdown .dropdown-list');
+    const toggleAllCheckbox = document.querySelector('.episode-dropdown .toggle-all');
+    const selectedOptionsContainer = document.querySelector('.episode-dropdown .selected-options');
     const selectedOptions = new Set();
 
     // Define the predetermined options
